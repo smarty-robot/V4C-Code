c714de57f0d3fc8b9e57d55c59c2e4b0a0bd4196
smartzeke <117780404+smartzeke@users.noreply.github.com>
2023-04-12 23:40:55 -0400

    converted to V4C

--- Torque Control Test 2/src/main.cpp
+++ Torque Control Test 2/src/main.cpp
@@ -25,8 +25,8 @@ void setup() {
   pinMode(13, INPUT); // wings
 }
 
-HallSensorAS5600 hallLeft(A6, 261, true);  // Create HallSensor object with pin number as argument
-HallSensorAS5600 hallRight(A5, -218, false);  // Create HallSensor object with pin number as argument
+HallSensorAS5600 hallLeft(A6, -55, false);  // Create HallSensor object with pin number as argument
+HallSensorAS5600 hallRight(A5, -115, false);  // Create HallSensor object with pin number as argument
 
 double drive_p = 100; // position
 double drive_i = 10; // integral
@@ -38,47 +38,54 @@ PID pidFL = PID(drive_dt, currentLimit, -currentLimit, drive_p, drive_d, drive_i
 PID pidBR = PID(drive_dt, currentLimit, -currentLimit, drive_p, drive_d, drive_i);
 PID pidFR = PID(drive_dt, currentLimit, -currentLimit, drive_p, drive_d, drive_i);
 
-PID pidWL = PID(.1, 1000, -1000, 100, 10, 30);
+PID pidWL = PID(.06, 1000, -1000, 100, 10, 0);
+PID pidWR = PID(.06, 1000, -1000, 100, 10, 0);
 
 
 int iterator = 0;
 int hold = 0;
 float maxRadPerSec = 50.0;
 
+boolean doVelocityControl = false;
+boolean doEffortControl = true;
+boolean doWings = true;
+
 void loop() {
 
     bus.PollCAN(); // Check for messages from the motors.
     //bus.CommandTorques(0, 0, 0, 0, C610Subbus::kOneToFourBlinks);
+    iterator++;
 
     long now = millis();
     if (now - last_command >= 10) // Loop at 100Hz. You should limit the rate at which you call CommandTorques to <1kHz to avoid saturating the CAN bus bandwidth
     {
         //Serial.println(now - last_command);
         last_command = millis();
-        int max = 100;
 
-        float leftIn = pulseIn(15, HIGH);
-        int leftPer = map(leftIn, 900, 2100, -max, max);
-        float leftVelTarget = map(leftPer, -100, 100, -maxRadPerSec, maxRadPerSec);
+        // Drive 
+        // Velocity Control
+        if(doVelocityControl){
+          float leftIn = pulseIn(15, HIGH);
+          int leftPer = map(leftIn, 900, 2100, -100, 100);
+          float leftVelTarget = map(leftPer, -100, 100, -maxRadPerSec, maxRadPerSec);
 
-        float rightIn = pulseIn(14, HIGH);
-        int rightPer = map(rightIn, 900, 2100, -max, max);
-        float rightVelTarget = map(rightPer, -100, 100, -maxRadPerSec, maxRadPerSec);
+          float rightIn = pulseIn(14, HIGH);
+          int rightPer = map(rightIn, 900, 2100, -100, 100);
+          float rightVelTarget = map(rightPer, -100, 100, -maxRadPerSec, maxRadPerSec);
 
-        double actualVel[4];
-        for (int i = 0; i < 4; i++){
-          actualVel[i] = bus.Get(i).Velocity();
-        }
-        int torqueMtr[4] = {0, 0, 0, 0};
-        torqueMtr[0] = pidBL.calculate(leftVelTarget, actualVel[0]);
-        torqueMtr[1] = pidFL.calculate(leftVelTarget, actualVel[1]);
-        torqueMtr[2] = pidBR.calculate(rightVelTarget, actualVel[2]);
-        torqueMtr[3] = pidFR.calculate(rightVelTarget, actualVel[3]);
+          double actualVel[4];
+          for (int i = 0; i < 4; i++){
+            actualVel[i] = bus.Get(i).Velocity();
+          }
+          int torqueMtr[4] = {0, 0, 0, 0};
+          torqueMtr[0] = pidBL.calculate(leftVelTarget, actualVel[0]);
+          torqueMtr[1] = pidFL.calculate(leftVelTarget, actualVel[1]);
+          torqueMtr[2] = pidBR.calculate(rightVelTarget, actualVel[2]);
+          torqueMtr[3] = pidFR.calculate(rightVelTarget, actualVel[3]);
 
-        // bus.CommandTorques(250,250,-250,-250, C610Subbus::kOneToFourBlinks);
-        bus.CommandTorques(torqueMtr[0], torqueMtr[1], torqueMtr[2], torqueMtr[3], C610Subbus::kOneToFourBlinks);
+          bus.CommandTorques(torqueMtr[0], torqueMtr[1], torqueMtr[2], torqueMtr[3], C610Subbus::kOneToFourBlinks);
 
-        if (false && iterator%5 == 0){//print maps
+          if (false && iterator%5 == 0){//print maps
           Serial.println();
 
           Serial.print("Time Past:      ");
@@ -113,21 +120,55 @@ void loop() {
           }
           Serial.println();
         }
-        iterator++;
-
-        float wingIn = pulseIn(13, HIGH);
-        float wingPosTarget = map(wingIn, 1100, 1900, 0, 180);
-        float wingPos = hallLeft.read();
-        int leftWingTorq = pidWL.calculate(wingPosTarget, wingPos);
-        bus.CommandTorques(-leftWingTorq, 0,0,0, C610Subbus::kFiveToEightBlinks);
-
-        if(iterator%50 == 0){
-          Serial.print("wing target positon:    ");
-          Serial.println(wingPosTarget);
-          Serial.print("Left Wing Position:     ");
-          Serial.println(wingPos);
-          Serial.print("Left Wing Torque:       ");
-          Serial.println(leftWingTorq);
         }
+        // Effort Control
+        if(doEffortControl){
+          float leftIn = pulseIn(15, HIGH);
+          float rightIn = pulseIn(14, HIGH);
+
+          int max_amperage = 16000;
+          int leftPer = map(leftIn, 900, 2100, -max_amperage, max_amperage);
+          int rightPer = map(rightIn, 900, 2100, -max_amperage, max_amperage);
+          
+          if(leftPer > -500 && leftPer < 500){
+            leftPer = 0;
+          }
+          if(rightPer > -500 && rightPer < 500){
+            rightPer = 0;
+          }
+
+          bus.CommandTorques(-leftPer, -leftPer, -rightPer, -rightPer, C610Subbus::kOneToFourBlinks);
+
+        }
+        
+        // Wing
+        // Wing Positioning      
+        if (doWings){
+          float wingIn = pulseIn(13, HIGH);
+          float wingPosTarget = map(wingIn, 1100, 1900, 0, 90);
+
+          float leftWingPos = hallLeft.read();
+          int leftWingTorq = pidWL.calculate(wingPosTarget, leftWingPos);
+
+          float rightWingPos = hallRight.read();
+          int rightWingTorq = pidWR.calculate(wingPosTarget, rightWingPos);
+
+          bus.CommandTorques(leftWingTorq, rightWingTorq,0,0, C610Subbus::kFiveToEightBlinks);
+
+          if(iterator%50 == 0){
+            Serial.println();
+            Serial.print("wing target positon:   ");
+            Serial.println(wingPosTarget);
+            Serial.print("Left Wing Position:    ");
+            Serial.println(leftWingPos);
+            Serial.print("Left Wing Torque:      ");
+            Serial.println(leftWingTorq);
+            Serial.print("Right Wing Position:   ");
+            Serial.println(rightWingPos);
+            Serial.print("Right Wing Torque:     ");
+            Serial.println(rightWingTorq);
+          }
+        }
+        
     }
 }
